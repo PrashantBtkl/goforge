0274561e8ecb7c0a438818796d947ac7dff4e1f2
PrashantBtkl <prashant44backup@gmail.com>
2024-09-20 19:51:16 +0530

    refactor: generate handlers template

--- example/handlers/get_users.go
+++ example/handlers/get_users.go
@@ -9,11 +9,10 @@ import (
 func (s *Server) GetUsers(c echo.Context) error {
 
 	response, err := s.Queries.GetUsers(c.Request().Context())
+
 	if err != nil {
 		s.Logger.Error("failed to execute sql", "err", err.Error())
 		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
 	}
-
 	return c.JSON(http.StatusOK, response)
-
 }
